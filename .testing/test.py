6c7f1f447d71113dd5b4d9b6a87b1133adcddd1d
Jacob Vanluven <armorgamesdot@gmail.com>
2025-02-25 21:23:38 -0500

    Version 1

--- .testing/test.py
+++ .testing/test.py
@@ -5,49 +5,78 @@ from PIL import Image
 
 from sgf_image_loader.sgf import SGF
 
-def calc_time(start: float) -> str:
-    return f"{round((time() - start) * 1000)}ms"
+class Tester:
+    def __init__(self):
+        self.colors = 0
+        self.time = 0
+        self.png_size = 0
+        self.sgf_size = 0
+        self.diff_size = 0
+        self.count = 0
 
-def test_image(png_in: str, sgf_out: str, png_out: str, depth: str):
-    print(f"{depth}{png_in.split("\\")[-1]}")
-    depth += "    "
+        self.test_folder('.testing\\png_in', '.testing\\sgf', '.testing\\png_out', '')
 
-    image = Image.open(png_in)
+        print("<tr>")
+        print("    <td>Average</td>")
+        print("    <td>-</td>")
+        print(f"    <td>{int(self.colors / self.count)} colors</td>")
+        print(f"    <td>{round(self.time / self.count, 2)}ms</td>")
+        print(f"    <td>{self.format_bytes(round(self.png_size / self.count))}</td>")
+        print(f"    <td>{self.format_bytes(round(self.png_size / self.count))}</td>")
+        print(f"    <td>{round(self.diff_size / self.count, 2):,.2f}%</td>")
+        print("</tr>")
 
-    sgf_out = sgf_out.replace('.png', '.sgf')
+    def format_bytes(self, bytes):
+        if bytes >= 1000000:
+            return f"{round(bytes / 1000000, 2)} MB"
+        if bytes >= 1000:
+            return f"{round(bytes / 1000, 2)} KB"
+        
+        return f"{bytes:,} B"
 
-    # image info
-    print(f"{depth}Image size  : {image.size}")
-    print(f"{depth}Colors used : {len(image.getcolors())}")
+    def calc_time(self, start: float) -> str:
+        return round((time() - start) * 1000)
 
-    # convert png to sgf
-    print(f"{depth}Converting  : ", end="")
-    t = time()
-    SGF.save_from_image(sgf_out, image)
-    print(calc_time(t))
+    def test_image(self, png_in: str, sgf_out: str, png_out: str, depth: str):
+        depth += "    "
 
-    # convert sgf to png
-    converted: Image = SGF.load_sgf(sgf_out)
-    converted.save(png_out)
+        image = Image.open(png_in)
 
-    # output sizes
-    print(f"{depth}png size    : {os.path.getsize(png_in)} Bytes")
-    print(f"{depth}sgv size    : {os.path.getsize(sgf_out)} Bytes")
-    print(f"{depth}Difference  : {(os.path.getsize(sgf_out) / os.path.getsize(png_in)) * 100:.2f}%")
+        sgf_out = sgf_out.replace('.png', '.sgf')
 
-def test_folder(png_in: str, sgf_out: str, png_out: str, depth: str):
-    print(f"{depth}Testing '{png_in}'")
+        # convert png to sgf
+        t = time()
+        SGF.save_from_image(sgf_out, image)
+        e = self.calc_time(t)
+        
+        # convert sgf to png
+        converted: Image = SGF.load_sgf(sgf_out)
+        converted.save(png_out)
 
-    for file in os.listdir(png_in):
-        if os.path.isdir(os.path.join(png_in, file)):
-            test_folder(os.path.join(png_in, file), os.path.join(sgf_out, file), os.path.join(png_out, file), depth + "    ")
-        else:
-            test_image(os.path.join(png_in, file), os.path.join(sgf_out, file), os.path.join(png_out, file), depth + "    ")
+        self.colors += len(image.getcolors())
+        self.time += e
+        self.png_size += os.path.getsize(png_in)
+        self.sgf_size += os.path.getsize(sgf_out)
+        self.diff_size += (os.path.getsize(sgf_out) / os.path.getsize(png_in)) * 100
+        self.count += 1
 
-def main():
-    test_folder('.testing\\png_in', '.testing\\sgf', '.testing\\png_out', '')
-    
+        print("<tr>")
+        print(f"    <td>{png_in.split("\\")[-1]}</td>")
+        print(f"    <td>{image.size}</td>")
+        print(f"    <td>{len(image.getcolors())} colors</td>")
+        print(f"    <td>{e}ms</td>")
+        print(f"    <td>{self.format_bytes(os.path.getsize(png_in))}</td>")
+        print(f"    <td>{self.format_bytes(os.path.getsize(sgf_out))}</td>")
+        print(f"    <td>{(os.path.getsize(sgf_out) / os.path.getsize(png_in)) * 100:,.2f}%</td>")
+        print("</tr>")
+
+    def test_folder(self, png_in: str, sgf_out: str, png_out: str, depth: str):
+        for file in os.listdir(png_in):
+            if os.path.isdir(os.path.join(png_in, file)):
+                self.test_folder(os.path.join(png_in, file), os.path.join(sgf_out, file), os.path.join(png_out, file), depth + "    ")
+            else:
+                self.test_image(os.path.join(png_in, file), os.path.join(sgf_out, file), os.path.join(png_out, file), depth + "    ")    
     
 
 if __name__ == "__main__":
-    main()
+    Tester()
